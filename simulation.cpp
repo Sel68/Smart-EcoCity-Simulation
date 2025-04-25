129e3182b42d57b7d1cf9ee74d1dc3502d53df26
sel68 <saadhasanellahie@gmail.com>
2025-04-25 17:32:24 +0500

    Making Class Transport

--- simulation.cpp
+++ simulation.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
 using namespace std;
+#include <iomanip>
 
-class Building {
+
+class Transport {
 protected:
-    double landCover;
+    double maintenanceCost, travelCost, cost;
+    int level;
+    double maintenanceState;
+    
 public:
-    Building(double landCover = 0) : landCover(landCover) {}
+
+    Transport(double mcost = 100, double tcost = 10, double mstate = 100.0, int lvl = 1, double buildCost = 2000)
+        : maintenanceCost(mcost), travelCost(tcost), maintenanceState(mstate), level(lvl), cost(buildCost) {}
+    
+    virtual ~Transport() {}
+    
+    double getMaintenanceState () const {return maintenanceState;}
+    double getCost() const { return cost; }
+    int getLevel() const {return level;}
+    double getMaintCost() const { return maintenanceCost; }
+
+    virtual void useTransport() {
+        maintenanceState -= 0.5 * (1 + (100.0 - maintenanceState)/100.0); //degradation accelerate when in poor condition
+        if (maintenanceState < 0) maintenanceState = 0;
+        cout << "Using generic transport. Maintenance: " << fixed << setprecision(1) << maintenanceState << "%" << endl;
+    }
 };
-
-class skyLineResidence : protected Building {
-    int noOfApartments;
-public:
-    skyLineResidence(int noOfApartments = 0) : Building(), noOfApartments(noOfApartments) {}
-};
-
-class house : protected Building {
-    int noOfRooms;
-public:
-    house(int noOfRooms = 0) : Building(), noOfRooms(noOfRooms) {}
-};
+    
 
 
-int main()
-{
+
+
+
+
+int main(){
 
     return 0;
 }
